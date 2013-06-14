bbb088713563d948d56abe702ffd961e1971c3fe
QETHAN <qbylucky@gmail.com>
2013-06-14 23:08:00 +0800

    基础框架初始化

---
+++ zjui/public/js/app.js
@@ -0,0 +1,14 @@
+// NOTICE!! DO NOT USE ANY OF THIS JAVASCRIPT
+// IT'S ALL JUST JUNK FOR OUR DOCS!
+// ++++++++++++++++++++++++++++++++++++++++++
+
+
+  $(function(){
+
+    var $window = $(window);
+      
+    // Disable certain links in docs
+    $('section [href^=#]').click(function (e) {
+      e.preventDefault();
+    });
+})
